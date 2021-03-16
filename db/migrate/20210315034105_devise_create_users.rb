954176deb1a4bc1c11fc6f9bbf8ae02f3dd9f038
Tsutomu-sasaki1 <tsutomusasaki1209@yahoo.co.jp>
2021-03-16 15:49:18 +0900

    ユーザー情報の登録
    gem 'active_hash'導入

--- db/migrate/20210315034105_devise_create_users.rb
+++ db/migrate/20210315034105_devise_create_users.rb
@@ -9,7 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :self_introduction,  null: false
-      t.integer :category_id,null: false
+      t.integer :category_id,       null: false
       
       ## Recoverable
       t.string   :reset_password_token
